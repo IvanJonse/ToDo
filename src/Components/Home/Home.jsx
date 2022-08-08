3d229d1db5c0376c98c50164bbd77ac9dcf01fe4
ivkn31 <ivkn31@gmail.com>
2022-08-08 14:01:33 +0700

    feat: refactor

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ import TodoItem from "./Item/TodoItem";
 import  check2  from '../../assets/check2.png'
 import  target  from '../../assets/target1.png'
 import Counter from "./Counter/Counter";
+import { couch } from "fontawesome";
 
 const data = [
     {   _id: 'eyujey8uuj',
@@ -37,40 +38,43 @@ export default function Home() {
 
     const removeTodo = (id) => setTodos([...todos].filter(t => t._id !== id))
 
-    const RemoveAllTodo = (id) => {
-        const copy = [...todos]
-        const current = copy.find(t => t._id === id)
-        if(current.isCompleted === true) {
-            return <></>
-        }
+    const removeAllTodo = () => {
+        setTodos([...todos].filter(t => t.isCompleted !== true))
+        setCount(0)
     }
+
 
     const divRef = useRef(null)
 
-    const btn = useRef(null)
-
     useEffect( () => {
-        changeTodo &&
+       
         divRef.current.scrollIntoView({ behavior: 'smooth', block: "end", inline: "end"})
     
       })
 
-      const scrollToTop = () => {
-        btn.current.scrollIntoView({ behavior: 'smooth', block: "start", inline: "start"});
-      };
+
 
     return (
-        <div className="text-white container-snap w-full h-full mx-auto overflow-y-auto relative">
-                <div ref={btn}></div>
-            <h1 className="text-2xl flex items-center justify-center mt-24 h-24 z-40 font-bold text-center sticky top-0 bg-gray-900">
-                <Counter counter={planed} image={target}/>
-                &ensp; ToDo for someone =) &ensp;
-                { todos.map(todo => RemoveAllTodo(todo._id)  )}
 
-                    <Counter counter={count} image={check2}/>
+        <div className="text-white container-snap w-full h-full mx-auto overflow-y-auto relative">
+
+            <h1 className="text-2xl flex items-center justify-center mt-24 h-24 z-40 font-bold text-center sticky top-0 bg-gray-900">
+
+                <Counter counter={planed} image={target}/>
+
+                &ensp; ToDo for someone =) &ensp;
+
+                <Counter counter={count} image={check2}/>
+              
+                { count > 1 ? <button className='ml-6' onClick={removeAllTodo}>Remove all</button> : ''}          
+
+           
             </h1>
-                <br /><br />
+
+            <br/><br/>
+
             <div ref={divRef} className="w-4/5 h-fill flex justify-items-center items-center mx-auto flex-col">
+
                 { todos.length === 0 ? 
                     <div className="text-4xl lg:text-3xl px-1 absolute top-1/2 flex items-center justify-items-center
                     font-bold tracking-wide mt-1/2 text-gray-800">
@@ -79,16 +83,23 @@ export default function Home() {
                 : 
             
                 <div className="w-full z-20 h-full mt-5 pb-40 flex justify-items-center items-center mx-auto flex-col">
-                    {
 
-                    todos.map(todo => <TodoItem key={todo._id} setPlaned={setPlaned} planed={planed} setCount={setCount} count={count} todo={todo} changeTodo={changeTodo} removeTodo={removeTodo}/>
+                    { todos.map((todo, item) => 
                     
-                    )}
+                    <TodoItem
+                        key={item}  
+                        setPlaned={setPlaned} 
+                        planed={planed} 
+                        setCount={setCount} 
+                        count={count} 
+                        todo={todo} 
+                        changeTodo={changeTodo} 
+                        removeTodo={removeTodo}
+                    />)}
+
                 </div>     
                 }
                
-                {/* {todos.length > 4 ? <button onClick={scrollToTop} className="border-none outline-none p-10 bg-green-500 text-white flex justify-center w-4 mx-auto text-center fixed bottom-6 right-12 z-30">ToTop</button> : ''} */}
-
             </div>
            <CreatedTodoField todos={todos} setPlaned={setPlaned} planed={planed} setCount={setCount} count={count} setTodos={setTodos}/>
         </div>
